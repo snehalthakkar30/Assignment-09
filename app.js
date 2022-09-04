fd24332880d0102a5b1f4caa9acf82915be97453
Karan Thakkar <karan@Karans-MacBook-Air.local>
2022-09-04 16:45:37 -0700

    b. Creating Node JS Server and committing

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! I am Snehal Manish Thakkar. This is a simple NodeJS Server Application!')
+})
+
+app.listen(port, () => {
+  console.log(`Application is listening on port ${port}`)
+})
